919e5c11d77536d4f42d11f0a8053d4614fd547e
sandromuggli <sandro.muggli@gmail.com>
2021-10-07 15:49:37 +0200

    solution ex1_racingCar

---
+++ src/com/company/RacingCar.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class RacingCar extends Car {
+    String team;
+    int number;
+    int horsePower;
+
+    @Override
+    public void drive() {
+        System.out.println("Ich fahre schnell.");
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+}
