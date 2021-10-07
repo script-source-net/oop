cbd0d899c8c7f7b00922bb3afaaf759034630889
sandromuggli <sandro.muggli@gmail.com>
2021-10-07 15:51:00 +0200

    solution ex1_racingCar

--- src/com/company/CarFactory.java
+++ src/com/company/CarFactory.java
@@ -9,17 +9,8 @@ public class CarFactory {
         bmw.setPrice(50000);
         bmw.drive();
 
-    /*
-        todo: implementiere ein RacingCar mit folgenden Eigenschaften:
-        - team (String)
-        - number (int)
-        - horsepower (int)
-        und der Methode drive, welche drive von Car überschreibt und "Ich fahre schnell." ausgibt,
-        ohne Car anzupassen!
-    */
-
         //todo: aktivieren diesen code:
-//        var racingCar = new RacingCar();
-//        racingCar.drive();
+        var racingCar = new RacingCar();
+        racingCar.drive();
     }
 }
