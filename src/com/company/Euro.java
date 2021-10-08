ab86daa5300252340dbc68934ce3f5472e83f71d
Gion <gion.desax@script-source.net>
2021-10-08 11:43:00 +0200

    Initial Commit

--- src/com/company/Euro.java
+++ src/com/company/Euro.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.logging.SocketHandler;
+
 public class Euro extends Waehrung{
     public static double kurs = 1;
     private double wert;
@@ -18,5 +20,25 @@ public class Euro extends Waehrung{
 
     public static void setEuroKurs(double Kurs){
         kurs =Kurs;
+    }
+
+    @Override
+    public String toString() {
+        return "" + euroBetrag() + " Euro";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Euro(1.23));
+        System.out.println(new Lire(1000));
+        Euro euro = new Euro(1023);
+        DM dm = new DM(1234.00);
+        System.out.println(euro instanceof DM);
+        System.out.println(euro instanceof Waehrung);
+        System.out.println(euro instanceof Euro);
+        System.out.println(euro instanceof Object);
+        System.out.println(dm instanceof DM);
+        System.out.println(dm instanceof Euro);
+        System.out.println(dm instanceof Waehrung);
+        System.out.println(dm instanceof Object);
     }
 }
